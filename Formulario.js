d0815fcb500434eba47458febac013cde73f585e
Cristhian Niño <saratos666@gmail.com>
2024-03-22 13:07:47 -0500

    Ejercicio 01 de Movil 2

---
+++ Formulario.js
@@ -0,0 +1,7 @@
+function Mensaje(){
+    var name= document.getElementById('name').value;
+    var email= document.getElementById('email').value;
+    var text= document.getElementById('text').value;
+    alert ('Hola '+name+' te enviaremos información a tu correo '+email+' sobre '+text+' apenas un supervisor le pueda ayudar.')
+
+}
